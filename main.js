b6537f614fc61f59d6cfc6bcb2f4682c8899f19f
josephrocca <1167575+josephrocca@users.noreply.github.com>
2022-03-05 22:52:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
-import { opine } from "https://deno.land/x/opine@2.1.2/mod.ts";
 import { parse } from "https://deno.land/std@0.128.0/flags/mod.ts"
-
 const args = parse(Deno.args);
-const app = opine();
+const port = args.port || 3000;
 
-app.all('/proxy', function(req, res) {
+const server = Deno.listen({ port });
+console.log(`Proxy server running. Use it like this:  http://localhost:${port}/?url=https://example.com`);
 
-  // Set CORS headers: allow all origins, methods, and headers
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+// Connections to the server will be yielded up as an async iterable.
+for await (const conn of server) {
+  // In order to not be blocking, we need to handle each connection individually without awaiting the function
+  serveHttp(conn);
+}
 
-  if(req.method === 'OPTIONS') { // CORS Preflight
-    res.send();
-  } else {
-    fetch(req.query.url).then(r => r.body.pipe(res));
+async function serveHttp(conn) {
+  // This "upgrades" a network connection into an HTTP connection.
+  const httpConn = Deno.serveHttp(conn);
+  // Each request sent over the HTTP connection will be yielded as an async iterator from the HTTP connection.
+  for await (const requestEvent of httpConn) {
+    try {
+      console.log("Handling:", requestEvent.request.url);
+      const queryParams = new URL(requestEvent.request.url).searchParams;
+      let url = queryParams.get("url");
+      if(!url) {
+        requestEvent.respondWith(new Response(""));
+        continue;
+      }
+
+      let headers = Object.fromEntries(requestEvent.request.headers.entries());
+      headers["Access-Control-Allow-Origin"] = "*";
+      headers["Access-Control-Allow-Methods"] = "GET, PUT, PATCH, POST, DELETE";
+      
+      let body = await fetch(url).then(r => r.body);
+      requestEvent.respondWith( new Response(body, {status:200, headers}) );
+    } catch(e) {
+      console.error(e);
+    }
   }
-  
-});
-
-const listener = app.listen(args.port || 3000, function() {
-  console.log('Proxy is listening on port ' + args.port || 3000);
-});
+}
